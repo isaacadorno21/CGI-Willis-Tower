270f814d43df21044ffcca8f49bb7218cf69d260
Isaac Adorno <isaacadorno21@gmail.com>
2019-05-20 22:19:03 +0300

    Add all files

---
+++ LightingScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightingScript : MonoBehaviour
+{
+    Light light;
+    bool sunny;
+    public Material sunBox;
+    public Material nightBox;
+    public GameObject snow;
+
+    // Start is called before the first frame updates   
+    void Start()
+    {
+        sunny = true;
+        light = GameObject.Find("mainLight").GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            if (sunny)
+            {
+                sunny = false;
+                RenderSettings.skybox = nightBox;
+                light.enabled = false;
+                snow.SetActive(true);
+                snow.GetComponent<ParticleSystem>().Play();
+            }
+            else
+            {
+                sunny = true;
+                RenderSettings.skybox = sunBox;
+                light.enabled = true;
+                snow.SetActive(false);
+                snow.GetComponent<ParticleSystem>().Stop();
+            }
+        }
+        if (sunny)
+        {
+            Vector3 rotationSpeed = new Vector3(-0.15f, 0, 0);
+            light.transform.Rotate(rotationSpeed);
+        }
+    }
+}
